a84518ade56c7fff7e917327b25c1164c9e1417a
IndianInvestor <90127254+IndianInvestor@users.noreply.github.com>
2021-09-05 15:02:01 +0800

    delete old contents

--- POSTS-CODE/MATLAB-PYTHON/eigFunc.m
+++
@@ -1,7 +0,0 @@
-function [V,D] = eigFunc(A)
-%returns diagonal matrix D of eigenvalues and matrix V 
-% whose columns are the corresponding right eigenvectors, 
-% so that A*V = V*D.
-[V, D] = eig(A);
-end
-
--- POSTS-CODE/MATLAB-PYTHON/test_eig.m
+++
@@ -1,4 +0,0 @@
-clear; close all; clc
-A = gallery('lehmer',4)
-
-[V,D] = eigFunc(A)
